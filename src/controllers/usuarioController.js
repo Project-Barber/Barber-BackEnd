a2f62eff49394df61d3e3c2d9a8e6050f76ac465
Roger.Carvalho <143417647+RogerCarvalhoUEPB@users.noreply.github.com>
2025-04-14 17:03:53 -0300

    feature userController userRoutes update

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -42,4 +42,66 @@ const loginUsuario = async (req, res) => {
   }
 };
 
-export { cadastrarUsuario, loginUsuario };
+const removerUsuario = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const result = await pool.query("DELETE FROM barber.usuarios WHERE id = $1 RETURNING *", [id]);
+
+    if (result.rowCount === 0) return res.status(404).json({ error: "Usuário não encontrado" });
+
+    res.status(200).json({ message: "Usuário removido com sucesso", usuario: result.rows[0] });
+  } catch (error) {
+    console.error("Erro ao remover usuário:", error);
+    res.status(500).json({ error: "Erro ao remover usuário" });
+  }
+}
+const todosOsUsuarios = async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM barber.usuarios");
+    const usuarios = result.rows;
+
+    if (usuarios.length === 0) return res.status(404).json({ error: "Nenhum usuário encontrado" });
+
+    res.status(200).json(usuarios);
+  } catch (error) {
+    console.error("Erro ao buscar usuários:", error);
+    res.status(500).json({ error: "Erro ao buscar usuários" });
+  }
+}
+
+const buscarUsuarioPorID = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const result = await pool.query("SELECT * FROM barber.usuarios WHERE id = $1", [id]);
+    const usuario = result.rows[0];
+
+    if (!usuario) return res.status(404).json({ error: "Usuário não encontrado" });
+
+    res.status(200).json(usuario);
+  } catch (error) {
+    console.error("Erro ao buscar usuário:", error);
+    res.status(500).json({ error: "Erro ao buscar usuário" });
+  }
+}
+
+const atualizarUsuario = async (req, res) => {
+  const { id } = req.params;
+  const { nome, data_nascimento, email, senha, endereco, telefone } = req.body;
+  try {
+    const hashedPassword = await bcrypt.hash(senha, 10);
+    const result = await pool.query(
+      "UPDATE barber.usuarios SET nome = $1, data_nascimento = $2, email = $3, senha = $4, endereco = $5, telefone = $6 WHERE id = $7 RETURNING *",
+      [nome, data_nascimento, email, hashedPassword, endereco, telefone, id]
+    );
+
+    if (result.rowCount === 0) return res.status(404).json({ error: "Usuário não encontrado" });
+
+    res.status(200).json({ message: "Usuário atualizado com sucesso", usuario: result.rows[0] });
+  } catch (error) {
+    console.error("Erro ao atualizar usuário:", error);
+    res.status(500).json({ error: "Erro ao atualizar usuário" });
+  }
+}
+export { cadastrarUsuario, loginUsuario, todosOsUsuarios,buscarUsuarioPorID, removerUsuario, atualizarUsuario };
--- src/routes/usuarioRoutes.js
+++ src/routes/usuarioRoutes.js
@@ -1,9 +1,13 @@
 import express from "express";
-import { cadastrarUsuario, loginUsuario } from "../controllers/usuarioController.js";
+import { cadastrarUsuario, loginUsuario, todosOsUsuarios,removerUsuario,atualizarUsuario,buscarUsuarioPorID } from "../controllers/usuarioController.js";
 
 const router = express.Router();
 
 router.post("/cadastrar", cadastrarUsuario);
 router.post("/login", loginUsuario);
+router.delete("/remover/:id", removerUsuario);
+router.get("/buscar", todosOsUsuarios);
+router.get("/buscar/:id", buscarUsuarioPorID);
+router.put("/atualizar/:id", atualizarUsuario);
 
 export default router;
